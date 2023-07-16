6d08b4c561526b9690b5988839a02d12ee7e722f
cmlglvz <camilo.glvz@gmail.com>
2023-07-15 23:52:53 -0400

    Updated data for micromonas y bathycoccus and added the coded for their exploration analysis

--- Code/exploration.R
+++ Code/exploration.R
@@ -2,13 +2,14 @@ library(tidyverse)
 library(viridis)
 library(hrbrthemes)
 library(ggpubr)
-library(ggTimeSeries)
+#library(ggTimeSeries)
+#library(streamgraph)
 
 
-data <- read.csv("./Data/copper_cytometry.csv", header = TRUE, sep = ";", skip = 0, skipNul = FALSE)
-data$Muestreo <- factor(data$Muestreo, levels = unique(data$Muestreo))
-data$Tiempo <- factor(data$Tiempo, levels = unique(data$Tiempo))
-data$Replica <- factor(data$Replica, levels = unique(data$Replica))
+#data <- read.csv("./Data/copper_cytometry.csv", header = TRUE, sep = ";", skip = 0, skipNul = FALSE)
+#data$Muestreo <- factor(data$Muestreo, levels = unique(data$Muestreo))
+#data$Tiempo <- factor(data$Tiempo, levels = unique(data$Tiempo))
+#data$Replica <- factor(data$Replica, levels = unique(data$Replica))
 
 luci <- read.csv("./Data/ostreococcus_lucimarinus.csv", header = TRUE, sep = ";", skip = 0, skipNul = FALSE)
 luci <- luci[,-7]
@@ -20,32 +21,252 @@ luci$Replica <- factor(luci$Replica, levels = unique(luci$Replica))
 long <- gather(luci[,c(2:5,7,9,11)], "Condition", "Counts", 5:7)
 long$Condition <- factor(long$Condition, levels = unique(long$Condition))
 
-ggplot(long, aes(x = Muestreo, y = Counts, group = Condition, color = Condition)) +
-  geom_line() +
+pl1 <- ggplot(long, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_line(linewidth = 1.5) + 
+  geom_point(size = 2) +
   scale_color_manual(values = c("#F94144", "#16DB93", "#0054A3")) +
-  ggtitle("Exploration") +
-  theme_light() +
+  geom_smooth(show.legend = TRUE, se = TRUE, linetype = "dashed", linewidth = 0.5) +
+  ggtitle("Ostreococcus lucimarinus") +
+  theme_ipsum_es() +
   ylab("Counts/mL")
+
+ggsave(plot = pl1, 
+       filename = "o_lucimarinus.tiff",
+       path = "./", 
+       width = 10,
+       dpi = 600)
 
 real <- gather(luci[,c(2:6,8,10)], "Condition", "Counts", 5:7)
 real$Condition <- factor(real$Condition, levels = unique(real$Condition))
 
-ggplot(real, aes(x = Muestreo,
-                 y = Counts,
-                 fill = Condition)) + 
+pl3 <- ggplot(real, 
+              aes(x = Muestreo,
+                  y = Counts,
+                  fill = Condition)) + 
   geom_boxplot() + 
   scale_fill_manual(values = c("#F94144", "#16DB93", "#0054A3")) + 
-  ggtitle("Exploration") + 
-  theme_light() + 
+  ggtitle("Ostreococcus lucimarinus boxplot version") + 
+  theme_ipsum_ps() + 
   ylab("Counts/mL")
 
-ggplot(longluci, aes(x = Muestreo, y = Counts)) + 
-  geom_point()
+ggsave(plot = pl3, 
+       filename = "o_lucimarinus_boxplot.tiff", 
+       path = "./", 
+       width = 10, 
+       dpi = 600)
 
+pl2 <- ggplot(real, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_point() +
+  scale_color_manual(values = c("#F94144", "#16DB93", "#0054A3")) +
+  geom_smooth() +
+  ggtitle("Ostreococcus lucimarinus jitter version") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pl2, 
+       filename = "o_lucimarinus_jitter.tiff",
+       path = "./", 
+       width = 10, 
+       dpi = 600)
 
 #filter <- dplyr::filter(longluci, Replica == 1 & Condition == "Cobre")
 #ggTimeSeries::ggplot_waterfall(dtData = filter, cXColumnName = "No_Muestreo", cYColumnName = "Counts")
 
+comm <- read.csv("./Data/micromonas_commoda.csv", header = TRUE, sep = ";", skip = 0, skipNul = FALSE)
+comm$Muestreo <- factor(comm$Muestreo, levels = unique(comm$Muestreo))
+comm$No_Muestreo <- factor(comm$No_Muestreo, levels = unique(comm$No_Muestreo))
+comm$Tiempo <- factor(comm$Tiempo, levels = unique(comm$Tiempo))
+comm$Replica <- factor(comm$Replica, levels = unique(comm$Replica))
+
+longc <- gather(comm[,c(2:5,7,9,11)], "Condition", "Counts", 5:7)
+longc$Condition <- factor(longc$Condition, levels = unique(longc$Condition))
+
+pc1 <- ggplot(longc, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_line(linewidth = 1.5) + 
+  geom_point(shape = 15, size = 2) +
+  scale_color_manual(values = c("#16DB93", "#F94144", "#0054A3")) +
+  geom_smooth(show.legend = TRUE, se = TRUE, linetype = "dashed", linewidth = 0.5) +
+  ggtitle("Micromonas commoda connected scatterplot") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pc1, 
+       filename = "m_commoda_scatter_connected.tiff",
+       path = "./Figures/", 
+       width = 10,
+       dpi = 600)
+
+realc <- gather(comm[,c(2:6,8,10)], "Condition", "Counts", 5:7)
+realc$Condition <- factor(realc$Condition, levels = unique(realc$Condition))
+
+pc2 <- ggplot(realc, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_point(shape = 15) +
+  scale_color_manual(values = c("#16DB93", "#F94144", "#0054A3")) +
+  geom_smooth() +
+  ggtitle("Micromonas commoda jitter version") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pc2, 
+       filename = "m_commoda_jitter.tiff",
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
+
+pc3 <- ggplot(realc, 
+              aes(x = Muestreo,
+                  y = Counts,
+                  fill = Condition)) + 
+  geom_boxplot() + 
+  scale_fill_manual(values = c("#16DB93", "#F94144", "#0054A3")) + 
+  ggtitle("Micromonas commoda boxplot version") + 
+  theme_ipsum() + 
+  ylab("Counts/mL")
+
+ggsave(plot = pc3, 
+       filename = "m_commoda_boxplot.tiff", 
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
+
+###############################################################################
+
+pras <- read.csv("./Data/bathycoccus_prasinos.csv", header = TRUE, sep = ";", skip = 0, skipNul = FALSE)
+pras$Muestreo <- factor(pras$Muestreo, levels = unique(pras$Muestreo))
+pras$No_Muestreo <- factor(pras$No_Muestreo, levels = unique(pras$No_Muestreo))
+pras$Tiempo <- factor(pras$Tiempo, levels = unique(pras$Tiempo))
+pras$Replica <- factor(pras$Replica, levels = unique(pras$Replica))
+
+longp <- gather(pras[,c(2:5,7,9,11)], "Condition", "Counts", 5:7)
+longp$Condition <- factor(longp$Condition, levels = unique(longp$Condition))
+
+pp1 <- ggplot(longp, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_line(linewidth = 1) + 
+  geom_point(shape = 17, size = 2) +
+  scale_color_manual(values = c("#16DB93", "#0054A3", "#F94144")) +
+  geom_smooth(show.legend = TRUE, se = TRUE, linetype = "dashed", linewidth = 0.5) +
+  ggtitle("Bathycoccus prasinos connected scatterplot version") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pp1, 
+       filename = "b_prasinos_scatter_connected.tiff",
+       path = "./Figures/", 
+       width = 10,
+       dpi = 600)
+
+realp <- gather(pras[,c(2:6,8,10)], "Condition", "Counts", 5:7)
+realp$Condition <- factor(realp$Condition, levels = unique(realp$Condition))
+
+pp2 <- ggplot(realp, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_point(shape = 17) +
+  scale_color_manual(values = c("#16DB93", "#0054A3", "#F94144")) +
+  geom_smooth() +
+  ggtitle("Bathycoccus prasinos jitter version") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pp2, 
+       filename = "b_prasinos_jitter.tiff",
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
+
+pp3 <- ggplot(realp, 
+              aes(x = Muestreo,
+                  y = Counts,
+                  fill = Condition)) + 
+  geom_boxplot() + 
+  scale_fill_manual(values = c("#16DB93", "#0054A3", "#F94144")) + 
+  ggtitle("Bathycoccus prasinos boxplot version") + 
+  theme_ipsum() + 
+  ylab("Counts/mL")
+
+ggsave(plot = pp3, 
+       filename = "b_prasinos_boxplot.tiff", 
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
+
+filter <- filter(realp, Condition != "Control")
+filterl <- filter(longp, Condition != "Control")
+
+pp4 <- ggplot(filterl, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_line(linewidth = 1) + 
+  geom_point(shape = 17, size = 2) +
+  scale_color_manual(values = c("#0054A3", "#F94144")) +
+  geom_smooth(show.legend = TRUE, se = TRUE, linetype = "dashed", linewidth = 0.5) +
+  ggtitle("Bathycoccus prasinos connected scatterplot version without Control group") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pp4, 
+       filename = "b_prasinos_scatter_connected_focused.tiff",
+       path = "./Figures/", 
+       width = 10,
+       dpi = 600)
+
+pp5 <- ggplot(filter, 
+              aes(x = Muestreo, 
+                  y = Counts, 
+                  group = Condition, 
+                  color = Condition)) +
+  geom_point(shape = 17) +
+  scale_color_manual(values = c("#0054A3", "#F94144")) +
+  geom_smooth() +
+  ggtitle("Bathycoccus prasinos jitter version without Control group") +
+  theme_ipsum() +
+  ylab("Counts/mL")
+
+ggsave(plot = pp5, 
+       filename = "b_prasinos_jitter_focused.tiff",
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
+
+pp6 <- ggplot(filter,
+              aes(x = Muestreo,
+                  y = Counts,
+                  fill = Condition)) + 
+  geom_boxplot() + 
+  scale_fill_manual(values = c("#0054A3", "#F94144")) + 
+  ggtitle("Bathycoccus prasinos boxplot version without Control group") + 
+  theme_ipsum() + 
+  ylab("Counts/mL")
+
+ggsave(plot = pp6, 
+       filename = "b_prasinos_boxplot_focused.tiff", 
+       path = "./Figures/", 
+       width = 10, 
+       dpi = 600)
 
 ggplot(filter, aes(x=year, y=n, group=name, color=name)) +
   geom_line() +
